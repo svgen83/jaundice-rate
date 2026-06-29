c172790445f3fa1a98b29cb38a3f125924fb7407
svgen83 <61458549+svgen83@users.noreply.github.com>
2026-06-29 11:15:16 +0400

    добавлен расчет рейтинга объективности

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import aiohttp
 import asyncio
+import pymorphy3
 from adapters.inosmi_ru import sanitize
+from text_tools import split_by_words, calculate_jaundice_rate
 
 
 async def fetch(session, url):
@@ -15,6 +17,9 @@ async def main():
         html = await fetch(session, 'https://inosmi.ru/20260629/ormuz-279091044.html')
         html_sanitized = sanitize(html)
         print(html_sanitized)
+        article_words = split_by_words(morph, html_sanitized)
+        rank = calculate_jaundice_rate(article_words, ['пролив', 'юридический'])
+        print(rank)
 
-
+morph = pymorphy3.MorphAnalyzer()
 asyncio.run(main())
